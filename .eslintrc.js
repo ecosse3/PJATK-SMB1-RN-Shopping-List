e7847a712111d00465172c80b5782a7b04a2267f
ecosse3 <theecosse@gmail.com>
2020-11-13 20:24:28 +0100

    deps: change eslintrc, prettierrc & deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,14 +34,17 @@ module.exports = {
     'import/no-extraneous-dependencies': 0,
     'import/no-unresolved': 0,
     'import/prefer-default-export': 0,
-    quotes: [2, 'single', {avoidEscape: true}],
+    quotes: [2, 'single', { avoidEscape: true }],
     'linebreak-style': 0,
-    'max-len': [0, {code: 120}],
+    'max-len': [0, { code: 120 }],
     'no-multiple-empty-lines': 0,
     'no-underscore-dangle': 'off',
     'no-unused-expressions': 0,
     'no-unused-vars': 'warn',
-    'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx', '.tsx']}],
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', '.tsx'] }
+    ],
     'react/jsx-no-target-blank': 0,
     'react/jsx-one-expression-per-line': 0,
     'react/jsx-props-no-spreading': 0,
@@ -49,6 +52,7 @@ module.exports = {
     'react/require-default-props': 0,
     'react/forbid-prop-types': 0,
     'react/destructuring-assignment': 0,
-    'object-curly-spacing': ['warn', 'always']
+    'react/display-name': 0,
+    'object-curly-spacing': ['warn', 'always'],
   }
 };
