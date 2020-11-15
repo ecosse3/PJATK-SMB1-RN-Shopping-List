1f6b020c3770413aed01a4bc26319327a885fffb
ecosse3 <theecosse@gmail.com>
2020-11-15 20:17:20 +0100

    feat(atoms): add more mockup products

--- src/store/atoms.tsx
+++ src/store/atoms.tsx
@@ -19,6 +19,49 @@ export const productListState = atom({
   key: 'productListState',
   default: [
     {
+      id: 1,
+      name: 'Jogurt naturalny',
+      price: 1.79,
+      amount: 2,
+      isBuy: false
+    },
+    {
+      id: 2,
+      name: 'Jogurt naturalny',
+      price: 1.79,
+      amount: 2,
+      isBuy: false
+    },
+    {
+      id: 3,
+      name: 'Jogurt naturalny',
+      price: 1.79,
+      amount: 2,
+      isBuy: false
+    },
+    {
+      id: 4,
+      name: 'Jogurt naturalny',
+      price: 1.79,
+      amount: 2,
+      isBuy: false
+    },
+    {
+      id: 5,
+      name: 'Jogurt naturalny',
+      price: 1.79,
+      amount: 2,
+      isBuy: false
+    },
+    {
+      id: 6,
+      name: 'Jogurt naturalny',
+      price: 1.79,
+      amount: 2,
+      isBuy: false
+    },
+    {
+      id: 7,
       name: 'Jogurt naturalny',
       price: 1.79,
       amount: 2,
