e35dfb32034a348e85e1e1bac5c044370ecc98c5
ecosse3 <theecosse@gmail.com>
2020-11-15 17:37:29 +0100

    feat(atoms): add product list state with mockup

--- src/store/atoms.tsx
+++ src/store/atoms.tsx
@@ -14,3 +14,15 @@ export const tabBarVisibleState = atom({
   key: 'tabBarVisibleState',
   default: false
 });
+
+export const productListState = atom({
+  key: 'productListState',
+  default: [
+    {
+      name: 'Jogurt naturalny',
+      price: 1.79,
+      amount: 2,
+      isBuy: false
+    }
+  ]
+});
