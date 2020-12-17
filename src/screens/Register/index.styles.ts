666e5aef291d1ff555567be17fe6e8e7f3ffcdb1
ecosse3 <theecosse@gmail.com>
2020-12-17 20:58:02 +0100

    feat(Register): add Register Screen and functionality

---
+++ src/screens/Register/index.styles.ts
@@ -0,0 +1,56 @@
+import styled, { css } from 'styled-components/native';
+import { TextInput } from 'react-native-paper';
+import { rgba } from 'polished';
+import { ThemeType } from '../../utils/types';
+
+interface IButtonsProps {
+  theme: ThemeType;
+  disabled?: boolean;
+  backgroundColor?: string;
+}
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+`;
+
+export const Text = styled.Text<{
+  size?: number;
+  color?: string;
+  button?: boolean;
+}>`
+  font-size: ${(props) => (props.size ? `${props.size}px` : '20px')};
+  font-weight: 500;
+  color: ${(props) =>
+    props.color ? props.color : props.button ? 'white' : 'black'};
+  padding: ${(props) => (props.button ? '0px' : '20px')};
+`;
+
+export const NameInput = styled(TextInput)`
+  height: 40px;
+  width: 80%;
+  margin-bottom: 10px;
+`;
+
+export const Button = styled.TouchableOpacity<IButtonsProps>`
+  background: ${(props) =>
+    props.disabled
+      ? rgba(props.theme.colors.primary, 0.5)
+      : props.theme.colors.primary};
+  width: 80%;
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+
+  ${(props) =>
+    props.backgroundColor &&
+    css`
+      background: ${props.backgroundColor};
+    `}
+`;
+
+export const WaveHand = styled.Text`
+  font-size: 48px;
+`;
---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { useSetRecoilState } from 'recoil';
+import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack';
+import auth from '@react-native-firebase/auth';
+import { Container, Text, Button, NameInput, WaveHand } from './index.styles';
+import { tabBarVisibleState } from '../../store';
+import { RegisterStackParamList, ThemeType } from '../../utils/types';
+
+interface IProps {
+  theme: ThemeType;
+}
+
+const RegisterScreen: React.FC<IProps> = (props: IProps) => {
+  const { theme } = props;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const setTabBarVisible = useSetRecoilState(tabBarVisibleState);
+
+  const navigation = useNavigation<StackNavigationProp<RegisterStackParamList>>();
+
+  const register = async () => {
+    try {
+      auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(() => {
+          console.log('Account created & user signed in!');
+          setTabBarVisible(true);
+          navigation.push('ShoppingListScreen');
+        })
+        .catch((error) => {
+          switch (error.code) {
+            case 'auth/invalid-email':
+              console.error('Adres email jest niepoprawny!');
+              break;
+
+            case 'auth/weak-password':
+              console.error('Wprowadzone hasło jest za słabe');
+              break;
+
+            case 'auth/email-already-in-use':
+              console.error('Ten adres email jest już zarejestrowany');
+              break;
+
+            default:
+              console.error(error);
+              break;
+          }
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Container>
+      <WaveHand>👋</WaveHand>
+      <Text>Zarejestruj się</Text>
+      <NameInput
+        theme={{
+          colors: {
+            primary: theme.colors.primary,
+            underlineColor: 'transparent'
+          }
+        }}
+        mode="outlined"
+        placeholder="Email"
+        onChangeText={(text) => setEmail(text)}
+        value={email}
+      />
+      <NameInput
+        theme={{
+          colors: {
+            primary: theme.colors.primary,
+            underlineColor: 'transparent'
+          }
+        }}
+        mode="outlined"
+        placeholder="Hasło"
+        secureTextEntry={true}
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+      />
+      <Button disabled={email.length === 0 || password.length === 0} onPress={() => register()}>
+        <Text button>Zarejestruj</Text>
+      </Button>
+    </Container>
+  );
+};
+
+const Stack = createStackNavigator<RegisterStackParamList>();
+
+const Register: React.FC<IProps> = ({ theme }: IProps) => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="RegisterScreen"
+        children={() => <RegisterScreen theme={theme} />}
+        options={{
+          header: () => null
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default Register;
