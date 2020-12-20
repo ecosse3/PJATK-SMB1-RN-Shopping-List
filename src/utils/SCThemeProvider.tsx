ef646a8920e186f23283dbb12c9f8c388457d6cc
ecosse3 <theecosse@gmail.com>
2020-12-20 20:27:14 +0100

    feat(SCThemeProvider): set user theme from API & add loading spinner

--- src/utils/SCThemeProvider.tsx
+++ src/utils/SCThemeProvider.tsx
@@ -1,35 +1,60 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components/native';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import AsyncStorage from '@react-native-community/async-storage';
+import firestore from '@react-native-firebase/firestore';
+import Spinner from 'react-native-loading-spinner-overlay';
 import { themes } from './theme';
 import Navigator from '../navigators/Navigator';
-import { themeState } from '../store';
+import { themeState, userState, loadingState } from '../store';
 
 const SCThemeProvider: React.FC = () => {
+  const [loading, setLoading] = useRecoilState(loadingState);
   const [currentTheme, setCurrentTheme] = useRecoilState(themeState);
+  const user = useRecoilValue(userState);
 
   useEffect(() => {
+    setLoading(true);
     const getTheme = async () => {
       try {
         const result = await AsyncStorage.getItem('@theme');
 
-        if (result === null) {
-          setCurrentTheme(0);
+        const userData = await firestore()
+          .collection('users')
+          .doc(user?.uid)
+          .get();
+
+        if (userData.data()) {
+          setCurrentTheme(userData.data().theme);
         } else {
+          setCurrentTheme(0);
+        }
+
+        if (result !== null && !userData.data()) {
           setCurrentTheme(parseInt(result, 10));
         }
+
+        setLoading(false);
       } catch (err) {
         console.log(err);
       }
     };
 
     getTheme();
-  }, []);
+  }, [user]);
 
   return (
     <ThemeProvider theme={themes[currentTheme]}>
-      <Navigator theme={themes[currentTheme]} />
+      {loading ? (
+        <Spinner
+          visible={loading}
+          textContent="Loading..."
+          overlayColor="rgba(255, 255, 255, 0.25)"
+          color="black"
+        />
+      ) : (
+        <Navigator theme={themes[currentTheme]} />
+      )}
     </ThemeProvider>
   );
 };
